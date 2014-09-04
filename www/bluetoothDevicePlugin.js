b48bf655963972aae54dcdf0d3ce292eaafd6c48
alex3016 <wuxianqun3016@gmail.com>
2014-09-05 07:43:15 +0800

    no message

--- www/bluetoothDevicePlugin.js
+++ www/bluetoothDevicePlugin.js
@@ -8,5 +8,13 @@ var bluetoothDevicePlugin = {
             [{test:"test"}]
         ); 
      }
+     getCurrentDevices:function(successCallback, errorCallback) {
+        cordova.exec(
+            successCallback, // success callback function
+            errorCallback, // error callback function
+            'BluetoothDevicePlugin', // mapped to our native Java class called "CalendarPlugin"
+            'getCurrentDevices', // with this action name
+        ); 
+     }
 }
 module.exports = bluetoothDevicePlugin;
