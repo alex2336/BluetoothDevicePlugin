c438a4c781b8ba521bb79ea515a802197ed2ada2
alex3016 <wuxianqun3016@gmail.com>
2014-09-04 23:22:06 +0800

    no message

--- src/android/BluetoothDevicePlugin.java
+++ src/android/BluetoothDevicePlugin.java
@@ -1,5 +1,5 @@
 package com.leotech.plugin;
-
+    
     import org.apache.cordova.CordovaPlugin;
     import org.apache.cordova.CallbackContext;
 
@@ -11,22 +11,66 @@ package com.leotech.plugin;
      * This class echoes a string called from JavaScript.
      */
     public class BluetoothDevicePlugin extends CordovaPlugin {
+        private DeviceManagerCallback mDelegate;    
+        private ArrayList<BleDevice> tempList=new ArrayList<BleDevice>();
+        private BleDeviceManager bleDeviceManager;
     	String ACTION_START_SCANNING = "startScanning";
+        String GET_CURRENT_DEVICES = "getCurrentDevices";
         @Override
         public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
             if (action.equals(ACTION_START_SCANNING)) {
-                String message = args.getString(0);
-                this.echo(message, callbackContext);
+                startScanning();
+                return true;
+            }else if(action.equals(GET_CURRENT_DEVICES){
+                listKnownDevices(callbackContext);
                 return true;
             }
             return false;
         }
+        private void startScanning(){
+            bleDeviceManager.setCallback(mDelegate);
+            bleDeviceManager.startScanning();
+        }
+        private void listKnownDevices(CallbackContext callbackContext) {
 
-        private void echo(String message, CallbackContext callbackContext) {
-            if (message != null && message.length() > 0) {
-                callbackContext.success(message);
-            } else {
-                callbackContext.error("Expected one non-empty string argument.");
+            JSONObject json = new JSONObject();
+            for(int i=0;i<tempList.size();i++){
+                json.put(tempList.get(i).getDeviceAddress(),tempList.get(i).getName());
             }
+            PluginResult result = new PluginResult(PluginResult.Status.OK, json);
+            callbackContext.sendPluginResult(result);
+        }
+        @Override
+        public void initialize(CordovaInterface cordova, CordovaWebView webView){
+            super.initialize(cordova, webView);
+            mDelegate=new DeviceManagerCallback(){
+                @Override
+                public void onDiscoverDevice(final LSDeviceInfo lsDevice) 
+                {
+                    if(lsDevice!=null&&!deviceExists(lsDevice.getDeviceName()))
+                    {
+                        BleDevice bleDevice=getBleDevice(
+                                   lsDevice.getDeviceName(),
+                                   lsDevice.getDeviceAddress(),
+                                   lsDevice.getDeviceType(),
+                                   lsDevice.getModelNumber());
+                        tempList.add(bleDevice);    
+                    }
+                }
+            }
+            bleDeviceManager=BleDeviceManager.getInstance(); 
+            bleDeviceManager.initialize(this.cordova.getActivity().getApplicationContext(),mDelegate);
+        }
+        private boolean deviceExists(String name) 
+        {
+            boolean found=false;
+            for (int i = 0; i < tempList.size(); i++) 
+            {
+              if (tempList.get(i).getName().equals(name)) 
+              {
+                return found=true;
+              }
+            }
+            return found;
         }
     }
