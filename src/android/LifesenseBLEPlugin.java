fba2b6291dfa17b3173ad8eb495cb8cb7e115393
alex3016 <wuxianqun3016@gmail.com>
2014-09-05 11:58:11 +0800

    '1'\

--- src/android/LifesenseBLEPlugin.java
+++ src/android/LifesenseBLEPlugin.java
@@ -18,12 +18,17 @@ import android.util.Log;
 	 */
 	public class LifesenseBLEPlugin extends CordovaPlugin {
 		private DeviceManagerCallback mDelegate;    
+		private LSDeviceInfo mDevice;
+		private LSDeviceInfo pairedDevice;
 		private ArrayList<BleDevice> tempList=new ArrayList<BleDevice>();
+		private ArrayList<BleDevice> deviceList=new ArrayList<BleDevice>();
 		private BleDeviceManager bleDeviceManager;
+		private TypeConversion typeConversion;
 		String ACTION_START_SCANNING = "startScanning";
 		String ACTION_STOP_SCANNING = "stopScanning";
 		String GET_CURRENT_DEVICES = "getCurrentDevices";
-
+		String PAIR_DEVICE = "pairDevice";
+		String GET_PARIED_DEVICE = "getPairedDevice";
 		@Override
 		public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 			if (action.equals(ACTION_START_SCANNING)) {
@@ -36,6 +41,12 @@ import android.util.Log;
 			}else if(action.equals(GET_CURRENT_DEVICES)){
 				listKnownDevices(callbackContext);
 				return true;
+			}else if(action.equals(PAIR_DEVICE)){
+				pairDevice(args.getString(0),args.getInt(1),args.getString(2),args.getString(3));
+				return true;
+			}else if(action.equals(GET_CURRENT_DEVICES)){
+				getPairedDevice(callbackContext);
+				return true;
 			}
 			return false;
 		}
@@ -46,13 +57,41 @@ import android.util.Log;
 		private void stopScanning(){
 			bleDeviceManager.stopScanning();
 		}
-		private void listKnownDevices(CallbackContext callbackContext) {
-
+		private void pairDevice(String name, int type, String address, String modelNumber){
+			mDevice=new LSDeviceInfo();
+			mDevice.setDeviceName(name);
+			mDevice.setDeviceType(typeConversion.integerToEnum(type));
+			mDevice.setDeviceAddress(address);
+			mDevice.setModelNumber(modelNumber);
+			stopScanning();
+			mDelegate=new DeviceManagerCallback(){
+				@Override
+				public void onPairedResults(final LSDeviceInfo device, final int state) {
+					if(device!=null&&state==0)
+					{
+						pairedDevice = device;
+					}
+				}
+			};
+			bleDeviceManager.toPairDevice(mDevice);
+		}
+		private void sendErrorMessage(CallbackContext callbackContext, String errorMessage) {
 			JSONObject json = new JSONObject();
 			try{
-				for(int i=0;i<tempList.size();i++){
+				json.put("error",errorMessage);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			PluginResult result = new PluginResult(PluginResult.Status.OK, json);
+			callbackContext.sendPluginResult(result);
+		}
+		private void listKnownDevices(CallbackContext callbackContext) {
 
-					json.put(tempList.get(i).getAddress(),tempList.get(i).getName());
+			JSONArray json = new JSONArray();
+			try{
+				for(int i=0;i<tempList.size();i++){
+					JSONObject obj = new JSONObject();
+					obj.put(tempList.get(i).getAddress(),tempList.get(i).getName());
 				}
 			}catch(Exception e){
 				e.printStackTrace();
@@ -60,9 +99,24 @@ import android.util.Log;
 			PluginResult result = new PluginResult(PluginResult.Status.OK, json);
 			callbackContext.sendPluginResult(result);
 		}
+		private void getPairedDevice(CallbackContext callbackContext) {
+
+			JSONObject json = new JSONObject();
+			try{
+				if(pairedDevice!=null){
+					json.put(pairedDevice.getDeviceAddress(),pairedDevice.getBroadcastID());
+				}
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+			
+			PluginResult result = new PluginResult(PluginResult.Status.OK, json);
+			callbackContext.sendPluginResult(result);
+		}
 		@Override
 		public void initialize(CordovaInterface cordova, CordovaWebView webView){
 			super.initialize(cordova, webView);
+			typeConversion=new TypeConversion();
 			mDelegate=new DeviceManagerCallback(){
 				@Override
 				public void onDiscoverDevice(final LSDeviceInfo lsDevice) 
@@ -78,6 +132,7 @@ import android.util.Log;
 					}
 				}
 			};
+			
 			bleDeviceManager=BleDeviceManager.getInstance(); 
 			bleDeviceManager.initialize(this.cordova.getActivity().getApplicationContext(),mDelegate);
 		}
@@ -90,7 +145,8 @@ import android.util.Log;
 			  {
 				return found=true;
 			}
+			}
+			return found;
 		}
-		return found;
+
 	}
-}
